c2f18fe10b233ea73a02331657407881fe4fbd3a
kylerhen <zqchen318@gmail.com>
2017-10-12 14:07:46 +0800

    update

--- catalog/view/theme/fastor/js/ssi-uploader.js
+++ catalog/view/theme/fastor/js/ssi-uploader.js
@@ -32,9 +32,11 @@
     };
     Ssi_upload.prototype.init = function (element) {
 		console.log("onload start="+this.options.design.front.img);
-		convertImgToDataURLviaCanvas(this.options.design.front.img,function(dataURL){
-			this.productImg = dataURL;
-		},"image/png");
+
+		this.productImg.src=this.options.design.front.img;	
+		this.productImg.onload=function(){
+			//console.log("onload end="+this.productImg.width);
+		};
         $(element).addClass('ssi-uploadInput')
          .after(this.$element = $('<div class="ssi-uploader">'));
         var $chooseBtn = $('' +
@@ -800,24 +802,6 @@
             }
         }
     };
-	
-	var convertImgToDataURLviaCanvas  = function(url, callback, outputFormat) {
-			var img = new Image();
-			img.crossOrigin = 'Anonymous';
-			img.onload = function() {
-			  var canvas = document.createElement('CANVAS');
-			  var ctx = canvas.getContext('2d');
-			  var dataURL;
-			  canvas.height = this.height;
-			  canvas.width = this.width;
-			  ctx.drawImage(this, 0, 0);
-			  dataURL = canvas.toDataURL(outputFormat);
-			  callback(dataURL);
-			  canvas = null;
-			};
-			img.src = url;
-	}
-	
     var setElementMessage = function (thisS, index, msgType, msg, title) {
         var className = '', elementSelector = 'table.ssi-imgToUploadTable', element;
         if (!thisS.options.preview) {
@@ -865,7 +849,7 @@
 				img.onload=function(){
 					if(n==1){
 						console.log(0,0,img.width,img.height,design.front.left,design.front.top,design.front.width,design.front.height);
-							ctx.drawImage(img,0,0,img.width,img.height,design.front.left,design.front.top,design.front.width,design.front.height);
+							ctx.drawImage(img,0,0,img.width,thisS.productImg.height,design.front.left,design.front.top,design.front.width,design.front.height);
 							element.attr('src', c.toDataURL("image/jpeg"));
 					}else{
 						var sx = 0;
@@ -907,9 +891,9 @@
 						//console.log( design.front.img);
 						//drawing(thisS, design, design.front.img,1,ctx,c,element,index);
 						if(thisS.productImg){
-							drawing(thisS, design, productImg,1,ctx,c,element,index);//console.log(0,0,thisS.productImg.width,thisS.productImg.height,design.front.left,design.front.top,design.front.width,design.front.height);
-							//ctx.drawImage(thisS.productImg,0,0,thisS.productImg.width,thisS.productImg.height,design.front.left,design.front.top,design.front.width,design.front.height);
-							//element.attr('src', c.toDataURL("image/jpeg"));
+							console.log(0,0,thisS.productImg.width,thisS.productImg.height,design.front.left,design.front.top,design.front.width,design.front.height);
+							ctx.drawImage(thisS.productImg,0,0,thisS.productImg.width,thisS.productImg.height,design.front.left,design.front.top,design.front.width,design.front.height);
+							element.attr('src', c.toDataURL("image/jpeg"));
 						}
 					}
 				}
